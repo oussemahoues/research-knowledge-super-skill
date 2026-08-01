54661195de6643efb5666117b4c788d50fefb6d8
oussemahoues <oussemahoues1@gmail.com>
2026-08-01 06:31:20 -0400

    Export v3 capability checks

--- src/evidence_research/runtime/__init__.py
+++ src/evidence_research/runtime/__init__.py
@@ -1,4 +1,8 @@
+from .capabilities import CapabilityCheck, evaluate_capabilities, parse_capabilities
 from .event_store import EventStore
 from .executor import DurableExecutor, TaskResult
 
-__all__ = ["EventStore", "DurableExecutor", "TaskResult"]
+__all__ = [
+    "CapabilityCheck", "evaluate_capabilities", "parse_capabilities",
+    "EventStore", "DurableExecutor", "TaskResult",
+]
